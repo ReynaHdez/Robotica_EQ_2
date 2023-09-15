49563320efbcd698ee86052bd62ec45e302df501
ReynaSantos501 <reynahdezsantos@gmail.com>
2023-09-14 21:49:12 -0600

    commit

--- P1_Carga_Visual_Img.py
+++ P1_Carga_Visual_Img.py
@@ -2,7 +2,7 @@ from keras.utils import load_img, img_to_array
 import matplotlib.pyplot as plt
 
 largo, alto = 500, 500
-file = 'Archivos\Archivos_Reyna\Foto1.jpg'
+file = 'perritos.jpg'
 
 img = load_img(file, target_size=(largo, alto),color_mode="grayscale")
 
